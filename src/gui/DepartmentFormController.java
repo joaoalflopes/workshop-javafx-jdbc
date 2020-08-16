e807957c7eb750e13d107a73be96abd894c849c2
joaoalflopes <68ronins@gmail.com>
2020-08-16 01:37:10 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,17 +3,24 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.database.DbException;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 	
 	private Department entity;
+	private DepartmentService service;
 	
 	@FXML
 	private TextField txtId;
@@ -30,18 +37,46 @@ public class DepartmentFormController implements Initializable {
 	@FXML
 	private Button btnCancel;
 	
+	
 	public void setDepartment(Department entity) {
 		this.entity = entity;
 	}
 	
-	@FXML
-	public void onBtnSalveAction() {
-		System.out.println("onBtnSalveAction");
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	@FXML
-	public void onBtnCancelAction() {
-		System.out.println("onBtnCancelAction");
+	public void onBtnSalveAction(ActionEvent event) {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+		entity = getFormData();
+		service.saveOrUpdate(entity);
+		Utils.currentStage(event).close();
+		
+		}
+		catch(DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		return obj;
+	
+	}
+
+	@FXML
+	public void onBtnCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 
 	@Override
